d7d3cb0116e3817c071c0363af8b8cd53e577c95
Edvin-Persson <edvin.persson@iths.se>
2021-03-22 11:49:17 +0100

    Uppgift 1 Step 2 - Created test for add multiple numbers

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,12 +3,16 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
     Calculator calculator = new Calculator();
+
+    // Step 1
 
     @Test
     @DisplayName("Adding two numbers")
@@ -31,6 +35,20 @@ public class CalculatorTest {
     void addingEmptyString() {
 
         assertEquals(0, calculator.add(""));
+
+    }
+
+
+    // Step 2
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3:6", "1,2,3,4:10", "1,2,3,4,5:15"}, delimiter = ':')
+    @DisplayName("adding multiply numbers")
+    void addingMultipleNumbers(String input, int expected) {
+
+        assertEquals(expected, calculator.add(input));
+
 
     }
 
