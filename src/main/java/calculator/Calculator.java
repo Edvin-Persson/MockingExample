0c146bc35be146dfa5f8e6234c81f559b77142d8
Edvin-Persson <edvin.persson@iths.se>
2021-03-22 11:14:35 +0100

    Uppgift 1 Step 1 - Created simple calculator with three tests

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,27 @@
+package calculator;
+
+public class Calculator {
+
+
+    public int add(String numbers) {
+
+        if (numbers.equals("")) {
+
+            return 0;
+
+        }
+
+
+        String[] arrayNumber = numbers.split(",");
+        int addingNumbers = 0;
+
+
+        for (int i = 0; i < arrayNumber.length; i++) {
+
+            addingNumbers += Integer.parseInt(arrayNumber[i]);
+
+        }
+
+        return addingNumbers;
+    }
+}
